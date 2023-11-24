8581a536d418a70c09528d63989f86384cd41b35
Alok Sandip Kale <alokkale2304@gmail.com>
2023-11-24 09:48:44 +0530

    Connect to mongodb database using mongoose

--- api/index.js
+++ api/index.js
@@ -1,14 +1,24 @@
 import express from 'express';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// const __dirname = dirname(fileURLToPath(import.meta.url))
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {console.log('Connected to the database')}).catch((err) => {console.log(err)});
 
 const app = express();
 const port = 3000
 
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/client/index.html')
+// })
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/index.html')
-})
+    res.send('<h1>Welcome</h1>')
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
