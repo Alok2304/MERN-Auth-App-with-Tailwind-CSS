0adcc21091679992058a8ae43a42cd9364905e09
Alok Sandip Kale <alokkale2304@gmail.com>
2023-11-28 12:16:30 +0530

    Fixed some bugs in signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,37 +1,39 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
-	const [formData, setFormData] = useState({});
-	const [error, setError] = useState(false);
-	const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+	
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
-	const handleChange = (e) => {
-		setFormData({ ...formData, [e.target.id]: e.target.value });
-	};
-
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-
-		try {
-			setLoading(true);
-			setError(false);
-			const res = await fetch("/api/auth/signup", {
-				method: "POST",
-				headers: { "Content-type": "application/json" },
-				body: JSON.stringify(formData),
-			});
-			const data = await res.json();
-			setLoading(false);
-			if (data.success === false) {
-				setError(true);
-				return;
-			}
-		} catch (error) {
-			setLoading(false);
-			setError(true);
-		}
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
 
 	return (
 		<div className="p-3 max-w-lg mx-auto">
