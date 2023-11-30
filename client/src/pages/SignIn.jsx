54fd7c4434edd9424803dfe99cfc4ec691a95def
Alok Sandip Kale <alokkale2304@gmail.com>
2023-11-30 15:30:19 +0530

    Add redux toolkit to handle global state

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,40 +1,44 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+	signInStart,
+	signInSuccess,
+	signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignIn() {
-  const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-	
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-  };
+	const [formData, setFormData] = useState({});
+	const { loading, error } = useSelector((state) => state.user);
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      setError(false);
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      setLoading(false);
-      if (data.success === false) {
-        setError(true);
-        return;
-      }
-      navigate("/");
-    } catch (error) {
-      setLoading(false);
-      setError(true);
-    }
-  };
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			dispatch(signInStart());
+			const res = await fetch("/api/auth/signin", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				dispatch(signInFailure(data));
+				return;
+			}
+			dispatch(signInSuccess(data));
+			navigate("/");
+		} catch (error) {
+			dispatch(signInFailure(error));
+		}
+	};
 
 	return (
 		<div className="p-3 max-w-lg mx-auto">
@@ -54,8 +58,11 @@ export default function SignIn() {
 					className="bg-slate-100 rounded-lg p-3"
 					onChange={handleChange}
 				></input>
-				<button disabled={loading} className="bg-slate-700 text-white uppercase p-3 rounded-lg disabled:opacity-80 hover:opacity-95">
-					{loading ? 'Loading...' : 'Sign In'}
+				<button
+					disabled={loading}
+					className="bg-slate-700 text-white uppercase p-3 rounded-lg disabled:opacity-80 hover:opacity-95"
+				>
+					{loading ? "Loading..." : "Sign In"}
 				</button>
 			</form>
 			<div className="mt-3">
@@ -66,7 +73,9 @@ export default function SignIn() {
 					</Link>
 				</p>
 			</div>
-			<p className="text-red-700 mt-4">{error && 'Something went wrong!'}</p>
+			<p className="text-red-700 mt-4">
+				{error ? error.message || "Something went wrong!" : ""}
+			</p>
 		</div>
 	);
 }
