8fd4b7835603fa0d9bc311a71f882f762095eed0
Alok Sandip Kale <alokkale2304@gmail.com>
2023-12-04 09:42:13 +0530

    Update header page amd made profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+	const { currentUser } = useSelector((state) => state.user);
+
 	return (
 		<div className="bg-blue-100">
 			<div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,8 +18,16 @@ export default function Header() {
 					<Link to="/about">
 						<li>About Us</li>
 					</Link>
-					<Link to="/signin">
-						<li>Sign In</li>
+					<Link to="/profile">
+						{currentUser ? (
+							<img
+								src={currentUser.profilePicture}
+								alt="profile Image"
+								className="h-7 w-7 rounded-full object-cover"
+							/>
+						) : (
+							<li>Sign In</li>
+						)}
 					</Link>
 				</ul>
 			</div>
